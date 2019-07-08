7ea12428e640094495d4df5c137128bd37bd5ccb
zmjiangi <zmjiangi@qq.com>
2019-07-08 16:08:11 +0800

    测试冲突

--- src/main/java/com/gzyijian/git/Goods.java
+++ src/main/java/com/gzyijian/git/Goods.java
@@ -3,6 +3,6 @@ package com.gzyijian.git;
 public class Goods {
 
 	public static void main(String[] args) {
-		System.out.println("rightÄãºÃ........");
+		System.out.println("right........");
 	}
 }
